34fffd846886afa91cbbb0f6fc2ec75853a17fbb
sbroad <sara.broad@aixplatform.com>
2020-01-11 21:21:15 -0500

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+import RegisterUser from './pages/Register'
+import TeacherPage from './pages/Teacher';
 // import { bindActionCreators } from 'redux';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Trading-Board</h1>
+        {/* <h1>Trading-Board</h1> */}
+        <Switch>
+          <Route exact path='/' />
+          <Route exact path='/account' component={RegisterUser} /> 
+          <Route exact path='/teachers' component={TeacherPage} /> 
+
+        </Switch>
       </div>
     )
   }
