31a93a8d16d1fd187456618e07e581b8a7b78cff
sbroad <sara.broad@aixplatform.com>
2020-02-03 20:32:41 -0500

    add set-up

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,4 +1,11 @@
 const actionTypes = {
+    AUTH: {
+        AUTH_ERROR: "AUTH_ERROR",
+        LOGIN_SUCCESS: "LOGIN_SUCCESS",
+        LOGIN_FAIL: "LOGIN_FAIL",
+        REGISTER_SUCCESS: "REGISTER_SUCCESS",
+        REGISTER_FAIL: "REGISTER_FAIL"
+    },
     POSTING: {
         GET_POSTINGS: "GET_POSTINGS",
         ADD_POSTINGS: "ADD_POSTINGS",
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,2 +1,6 @@
 const router = require("express").Router();
+const authRoutes = require('./auth-routes');
 
+router.use("/login", authRoutes);
+
+module.exports = router;
