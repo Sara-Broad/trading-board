380cb63d28cb743973ecec163e8014d5603150c3
sbroad <sara.broad@aixplatform.com>
2020-05-17 16:35:52 -0400

    working components

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -23,96 +23,6 @@ class RegisterUser extends Component {
             }
         }
 
-
-// function selectedSubreddit(state = 'reactjs', action) {
-//   switch (action.type) {
-//     case SELECT_SUBREDDIT:
-//       return action.subreddit
-//     default:
-//       return state
-//   }
-// }
-
-// function posts(
-//   state = {
-//     isFetching: false,
-//     didInvalidate: false,
-//     items: []
-//   },
-//   action
-// ) {
-//   switch (action.type) {
-//     case INVALIDATE_SUBREDDIT:
-//       return Object.assign({}, state, {
-//         didInvalidate: true
-//       })
-//     case REQUEST_POSTS:
-//       return Object.assign({}, state, {
-//         isFetching: true,
-//         didInvalidate: false
-//       })
-//     case RECEIVE_POSTS:
-//       return Object.assign({}, state, {
-//         isFetching: false,
-//         didInvalidate: false,
-//         items: action.posts,
-//         lastUpdated: action.receivedAt
-//       })
-//     default:
-//       return state
-//   }
-// }
-
-// function postsBySubreddit(state = {}, action) {
-//   switch (action.type) {
-//     case INVALIDATE_SUBREDDIT:
-//     case RECEIVE_POSTS:
-//     case REQUEST_POSTS:
-//       return Object.assign({}, state, {
-//         [action.subreddit]: posts(state[action.subreddit], action)
-//       })
-//     default:
-//       return state
-//   }
-// }
-
-// const rootReducer = combineReducers({
-//   postsBySubreddit,
-//   selectedSubreddit
-// })
-
-// export default rootReducer
-    
-//         function fetchPosts(subreddit) {
-//             return dispatch => {
-//               dispatch(requestPosts(subreddit))
-//               return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-//                 .then(response => response.json())
-//                 .then(json => dispatch(receivePosts(subreddit, json)))
-//             }
-//           }
-          
-//           function shouldFetchPosts(state, subreddit) {
-//             const posts = state.postsBySubreddit[subreddit]
-//             if (!posts) {
-//               return true
-//             } else if (posts.isFetching) {
-//               return false
-//             } else {
-//               return posts.didInvalidate
-//             }
-//           }
-          
-//           export function fetchPostsIfNeeded(subreddit) {
-//             return (dispatch, getState) => {
-//               if (shouldFetchPosts(getState(), subreddit)) {
-//                 return dispatch(fetchPosts(subreddit))
-//               }
-//             }
-//           }
-
-
-
     componentDidMount() {
         const { users } = this.props
         if (users.length === 0) {
@@ -149,7 +59,6 @@ class RegisterUser extends Component {
 }
 
 RegisterUser.propTypes = {
-    // user: PropTypes.array.isRequired,
     users: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired,
     loadUsers: PropTypes.func.isRequired
